299e721816855dca2d36c41ffeb743bec3680c71
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-02-08 14:45:16 +0100

    Adds imgs + general classes

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,47 @@ export default {
 </script>
 
 <style lang="scss">
+// Variables
+$blue: #0282f9;
+$white: #fff;
+$black: #1c1c1c;
+
+// Presets
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+img {
+  max-width: 100%;
+}
+
+// Utilities
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+
+// Text Colors
+.text-blue {
+  color: $blue;
+}
+.text-white {
+  color: $white;
+}
+.text-black {
+  color: $black;
+}
+
+// Background Colors
+.bg-blue {
+  background: $blue;
+}
+.bg-white {
+  background: $white;
+}
+.bg-black {
+  background: $black;
+}
 </style>
